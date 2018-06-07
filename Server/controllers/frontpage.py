280e58928b9b18c1c97f955a2d8652b8223f5fb5
Andis Cirulis <andis.cirulis@whitedigital.eu>
2018-06-07 23:25:35 +0300

    different updates

--- Server/controllers/frontpage.py
+++ Server/controllers/frontpage.py
@@ -5,5 +5,25 @@ frontpage = Blueprint('frontpage', __name__)
 
 @frontpage.route('/')
 def index():
-    data = {'key': 'value'}
-    return render_template('index.html', key=data)
+    banks = [
+        {'logo': 'swedbank.png',
+         'title': 'Swedbank',
+         'api': 'Beta v1.0',
+         'api_url': 'https://www.swedbank.com/openbanking/',
+         'login_url': '#',
+         },
+        {'logo': 'seb.svg',
+         'title': 'SEB',
+         'api': 'v1.0',
+         'api_url': 'https://developer.baltics.sebgroup.com/landing',
+         'login_url': '#',
+         },
+        {'logo': 'nordea.png',
+         'title': 'NORDEA',
+         'api': 'v2.2',
+         'api_url': 'https://developer.nordeaopenbanking.com/',
+         'login_url': '#',
+         },
+
+    ]
+    return render_template('index.html', banks=banks)
--- Server/webrouter.py
+++ Server/webrouter.py
@@ -1,15 +1,21 @@
 from flask import Flask
+from controllers import *
+import sys
 
-application = Flask(__name__)
+application = Flask(__name__, static_folder='assets')
 application.jinja_env.autoescape = False
 
-from controllers.frontpage import frontpage
-from controllers.login import login
-from controllers.tests import tests
+# simple autoloader for controller routes
+modules = sys.modules.keys()
+for module in modules:
+    parts = module.split('.')
+    if len(parts) == 2 and parts[0] == 'controllers':
+        mod = parts[1]
+        url = '' if mod == 'frontpage' else mod  # exception for index page - does not have extra url
+        application.register_blueprint(getattr(sys.modules['controllers.' + mod], mod), url_prefix='/' + url)
 
-application.register_blueprint(frontpage, url_prefix='/')
-application.register_blueprint(login, url_prefix='/login')
-application.register_blueprint(tests, url_prefix='/tests')
+# application.register_blueprint(frontpage.frontpage, url_prefix='/')
+# application.register_blueprint(login.login, url_prefix='/login')
 
 if __name__ == "__main__":
     application.run(host='178.62.77.129', port=8080, debug=True, threaded=True)
